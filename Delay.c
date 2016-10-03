fccddf5fe9f1a599c3ea5552732209795381d05b
Yasin Cetin <yasincetin1@gmail.com>
2016-10-03 04:23:48 +0300

    Update Delay.c

--- Delay.c
+++ Delay.c
@@ -6,10 +6,10 @@ void Delay100ms(unsigned long time)
 {
   unsigned long i;
   while(time > 0)
-	{
+  {
     i = 1333333; // means 100ms
     while(i > 0)
-		{
+    {
       i = i - 1;
     }
     time = time - 1; 
