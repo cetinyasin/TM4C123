9e22e67c83616bee66674bf03d74179f60df0623
Yasin Cetin <yasincetin1@gmail.com>
2016-10-04 05:17:34 +0300

    Update PLL.c

--- PLL.c
+++ PLL.c
@@ -12,7 +12,7 @@ void PLL_Init(void)
 	SYSCTL_RCC2_R |= SYSCTL_RCC2_BYPASS2;
 	// 2nd step; specify the crystal frequency by setting XTAL bits
 	// SET OSCSRC2 (XTAL freq. table)
-	SYSCTL_RCC_R &= ~SYSCTL_RCC_XTAL_M;		   // clear XTAL field,
+	SYSCTL_RCC_R &= ~SYSCTL_RCC_XTAL_M;	 // clear XTAL field,
 	SYSCTL_RCC_R |= SYSCTL_RCC_XTAL_16MHZ;   // configure for 16MHz crystal
 	SYSCTL_RCC2_R &= ~SYSCTL_RCC2_OSCSRC2_M; // clear oscillator source
 	SYSCTL_RCC2_R |= SYSCTL_RCC2_OSCSRC2_MO; // configure for Main Oscillator source
@@ -23,7 +23,7 @@ void PLL_Init(void)
 	// then the clock will be divided by n+1.(to get 80MHz from 400Mhz, n=4 so 400Mhz/(4+1)=80Mhz)
 	SYSCTL_RCC2_R |= SYSCTL_RCC2_DIV400; // 400MHz PLL
 //	SYSCTL_RCC2_R = (SYSCTL_RCC2_R&~0x1FC00000); // clear bits 28:23(SYSDIV2)
-  SYSCTL_RCC2_R |= 4<<22;			 // (SYSDIV2=4)
+	SYSCTL_RCC2_R |= 4<<22;			 // (SYSDIV2=4)
 	// 5th step;
 	// Wait SYSCTL_RIS_R to become high
 	while((SYSCTL_RIS_R & SYSCTL_RIS_PLLLRIS)==0);
