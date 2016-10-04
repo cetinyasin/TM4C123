ae13cb21c23e1bad4ec22ccb6260f7d8e54a3cc4
Yasin Cetin <yasincetin1@gmail.com>
2016-10-05 01:08:55 +0300

    Update ADC.h

--- ADC.h
+++ ADC.h
@@ -1,3 +1,10 @@
+// Sample rate 125k samples/second
+// SS3 triggering event: software trigger
+// SS3 1st sample source:  channel 1
+// SS3 interrupts: enabled but not promoted to controller
+// PE2 - AIN1
 
 void ADC_Init(void);
+
+// busy wait sync
 unsigned long ADC0_IN(void);
